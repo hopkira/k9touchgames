2cba266fbd405c5eec7570a07edea28c21f4ed42
Richard Hopkins <hopkira@googlemail.com>
2022-11-14 22:49:05 +0000

    Detecting mousbutton clicks on tiles

--- anagram.py
+++ anagram.py
@@ -5,7 +5,9 @@ pygame.init()
 width  = 1024
 height = 786
 
-size = (width,  height)
+size = 150
+
+win_size = (width,  height)
 
 # Define some colors
 BLACK = ( 0, 0, 0)
@@ -14,12 +16,12 @@ GREEN = ( 0, 255, 0)
 RED = ( 255, 0, 0)
 
 # light shade of the button
-color_light = (170,170,170)
+colour_light = (170,170,170)
   
 # dark shade of the button
-color_dark = (100,100,100)
+colour_dark = (100,100,100)
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(win_size)
 
 pygame.display.set_caption("Anagram Game")
 
@@ -29,23 +31,40 @@ carryOn = True
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
-def draw_word(word:str, size:int, line:int) -> list:
-    rects = []
+def draw_letter(letter:str, size:int, row:int, col:int, colour:tuple) -> list:
     letterfont = pygame.font.SysFont('Corbel',size)
-    for num,letter in enumerate(word):
-        text = letterfont.render(letter,True,WHITE)
-        text_rect = text.get_rect(center = (width/2 + ((num-2.5) * (size * 1.1)), line * size * 1.1))
-        # pygame.draw.rect(screen, RED, text_rect, 0)
-        rect  = (((width/2) -size/2 + ((num-2.5) * size * 1.1)), -size/2 + line * size * 1.1, size, size)
-        pygame.draw.rect(screen, WHITE, rect, width=3, border_radius=7)
-        rect = tuple(letter) + rect
+    # for num,letter in enumerate(word):
+    text = letterfont.render(letter,True,WHITE)
+    text_rect = text.get_rect(center = (width/2 + ((col-2.5) * (size * 1.1)), row * size * 1.1))
+    # pygame.draw.rect(screen, RED, text_rect, 0)
+    rect  = (int((width/2) -size/2 + ((col-2.5) * size * 1.1)), int(-size/2 + row * size * 1.1), size, size)
+    pygame.draw.rect(screen, colour, rect, width=0, border_radius=7)
+    rect = tuple(letter) + rect
+    screen.blit(text, text_rect)
+    return rect
+
+def draw_word(word:str,size:int, row:int,col: int, colour:tuple) -> list:
+    rects = []
+    for col,letter in enumerate(list(word)):
+        rect = draw_letter(letter, size, row, col, colour)
         rects.append(rect)
-        screen.blit(text, text_rect)
     return rects
 
+def touch_spot(touch_spots) -> str:
+    for num,spots in enumerate(touch_spots):
+        letter,left,top,box_width,box_height = touch_spots[num]
+        if left <= mouse[0] <= left + box_width and top <= mouse[1] <= top + box_height:
+            pygame.draw.rect(screen,colour_light,[left, top, box_width, box_height],border_radius=7)
+            letterfont = pygame.font.SysFont('Corbel',int(size*1.1))
+            # for num,letter in enumerate(word):
+            text = letterfont.render(letter,True,BLACK)
+            text_rect = text.get_rect(center = ( left + box_width/2, top + box_height/2  ))
+            screen.blit(text, text_rect)
+            return letter
+
 touch_spots = []
-touch_spots = touch_spots + draw_word("WORDLE", 150, 1)
-touch_spots  = touch_spots + draw_word("INPUT_", 150, 2)
+touch_spots = touch_spots + draw_word("WORDLE", 150, 1, 0, colour_dark)
+touch_spots  = touch_spots + draw_word("INPUT_", 150, 2, 0, colour_dark)
 # -------- Main Program Loop -----------
 
 while carryOn:
@@ -54,9 +73,7 @@ while carryOn:
         if event.type == pygame.QUIT: # If user clicked close
             carryOn = False # Flag that we are done so we can exit the while loop
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-                print(touch_spots)
-                pygame.quit()
+            print(touch_spot(touch_spots))
  
      # --- Game logic should go here
     mouse = pygame.mouse.get_pos()
@@ -67,13 +84,14 @@ while carryOn:
     #pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
     #pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
     #pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-        pygame.draw.rect(screen,color_light,[width/2,height/2,140,40])
-    else:
-        pygame.draw.rect(screen,color_dark,[width/2,height/2,140,40])
-    draw_word("WORDLE", 150, 1)
-    draw_word("INPUT_", 150, 2)
+    #if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+    #    pygame.draw.rect(screen,colour_light,[width/2,height/2,140,40])
+    #else:
+    #    pygame.draw.rect(screen,colour_dark,[width/2,height/2,140,40])
+    draw_word("WORDLE", 150, 1, 0, colour_dark)
+    draw_word("INPUT_", 150, 2, 0, colour_dark)
 
+    touch_spot(touch_spots)
     # --- Go ahead and update the screen with what we've drawn.
     #pygame.display.flip(
     pygame.display.update()
