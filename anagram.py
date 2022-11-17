f87bc76426f105f4b60dcb287a7874f4745357cb
Richard Hopkins <hopkira@googlemail.com>
2022-11-17 20:57:40 +0000

    Refactoring

--- anagram.py
+++ anagram.py
@@ -11,6 +11,7 @@ GREEN = ( 0, 255, 0)
 DARK_GREEN = ( 0, 132, 74)
 BR_GREEN = (0, 66, 37)
 RED = ( 255, 0, 0)
+YELLOW  = (255,255,0)
 
 # light shade of the button
 colour_light = (170,170,170)
@@ -33,52 +34,116 @@ carryOn = True
 # The clock will be used to control how fast the screen updates
 clock = pygame.time.Clock()
 
-def draw_letter(letter:str, size:int, row:int, col:int, colour:tuple) -> list:
-    letterfont = pygame.font.SysFont('Corbel',size)
-    # for num,letter in enumerate(word):
-    text = letterfont.render(letter,True,DARK_GREEN)
-    text_rect = text.get_rect(center = (width/2 + ((col-2.5) * (size * 1.1)), row * size * 1.1))
-    # pygame.draw.rect(screen, RED, text_rect, 0)
-    rect  = (int((width/2) -size/2 + ((col-2.5) * size * 1.1)), int(-size/2 + row * size * 1.1), size, size)
-    pygame.draw.rect(screen, colour, rect, width=5, border_radius=7)
-    rect = tuple(letter) + rect
-    screen.blit(text, text_rect)
-    return rect
 
-def draw_word(word:str,size:int, row:int,col:int, colour:tuple) -> list:
-    rects = []
-    start_col = col
-    for col,letter in enumerate(list(word)):
-        rect = draw_letter(letter, size, row, col + start_col, colour)
-        rects.append(rect)
-    return rects
+class Letter():
+    '''
+    Refactored letter class
+    '''
+    def __init__(self, letter:str, size:int, row:int, col:int, colour: tuple)  -> tuple:
+        self.letter = letter
+        self.size = size
+        self.row = row
+        self.col = col
+        self.colour = colour
+        self.rect  = (int((width/2) -size/2 + ((col-2.5) * size * 1.1)), int(-size/2 + row * size * 1.1), size, size)
 
-def touch_spot(touch_spots) -> str:
-    for num,spots in enumerate(touch_spots):
-        letter,left,top,box_width,box_height = touch_spots[num]
+    def draw(self, colour: tuple):
+        letterfont = pygame.font.SysFont('Corbel',size)
+        text = letterfont.render(self.letter,True, colour)
+        text_rect = text.get_rect(center = (width/2 + ((self.col-2.5) * (self.size * 1.1)), self.row * self.size * 1.1))
+        pygame.draw.rect(screen, colour, self.rect, width = 5, border_radius = 7)
+        screen.blit(text, text_rect)
+
+    def is_touched(self) -> bool:
+        left,top,box_width,box_height = self.rect
         if left <= mouse[0] <= left + box_width and top <= mouse[1] <= top + box_height:
-            pygame.draw.rect(screen,DARK_GREEN,[left, top, box_width, box_height], width=5,border_radius=7)
-            letterfont = pygame.font.SysFont('Corbel',int(size))
-            # for num,letter in enumerate(word):
-            text = letterfont.render(letter,True,GREEN)
-            text_rect = text.get_rect(center = ( left + box_width/2, top + box_height/2  ))
-            screen.blit(text, text_rect)
-            return letter
+            self.draw(colour = GREEN)
+            return True
+        else:
+            self.draw(colour = self.colour)
+            return False
 
-touch_spots = []
-touch_spots = touch_spots + draw_word("WORDLE", 150, 2, 0, colour_dark)
-touch_spots = touch_spots + draw_word("INPUT_", 150, 3, 0, colour_dark)
-touch_spots = touch_spots + draw_word("PASS", 150, 4, 1, RED)
+
+class Word():
+    '''
+    Refactored word class
+    '''
+    def __init__(self,word:str,size:int, row:int,col:int, colour:tuple):
+        self.word = word
+        self.size = size
+        self.row = row
+        self.col = col
+        self.colour = colour
+
+    def draw(self) -> list:
+        word_list = []
+        for col,letter in enumerate(list(self.word)):
+            new_letter = Letter(letter, self.size, self.row, self.col + col, self.colour)
+            new_letter.draw(self.colour)
+            word_list.append(new_letter)
+        return word_list
+
+
+class Timer():
+    '''
+    Timer
+    '''
+    def __init__(self):
+        self.timer = 1000
+        self.timer_size = size * 6 * 1.1
+        self.colour = GREEN
+
+    def draw(self):
+        time = self.timer / 1000
+        if time < 0.333:
+            self.colour = RED
+        if time  >= 0.333 and time <= 0.666:
+            self.colour = YELLOW
+        if time > 0.666:
+            self.colour = GREEN
+        pygame.draw.rect(screen, DARK_GREEN, [int((width/2)-3*(size*1.1)), int((5*size)), int(self.timer_size), 80], width = 5, border_radius = 7)
+        pygame.draw.rect(screen, BLACK, [int(width/2)-((3*(size*1.1))-20), int(5*size+20), int(self.timer_size-40), 40], border_radius = 7)
+        pygame.draw.rect(screen, self.colour, [int((width/2)-(time*(3*(size*1.1))-20)), int(5*size+20), int((self.timer_size-40) * time), 40], border_radius = 7)
+    
+    def timer_expired(self, amount:int = 1) -> bool:
+        self.timer = self.timer - amount
+        if self.timer > 0:
+            return False
+        else:
+            return True
+
+    def reset_timer(self):
+        self.timer = 100
+
+
+# Draw board 
+screen.fill(BLACK)
+
+score_line = Word("000000",150,1,0,BR_GREEN)
+anagram_line = Word("      ", 150, 2, 0, DARK_GREEN)
+input_line = Word("MASTER", 150, 3, 0, GREEN)
+lives_line = Word("LIVES3", 150, 4, 0, DARK_GREEN)
+lines  = []
+
+score_word = score_line.draw()
+anagram_word = anagram_line.draw()
+input_word = input_line.draw()
+livess_word = lives_line.draw()
+
+timer = Timer()
 
 # -------- Main Program Loop -----------
-
+time = 1
 while carryOn:
     # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             carryOn = False # Flag that we are done so we can exit the while loop
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(touch_spot(touch_spots))
+            for letter in input_word:
+                if letter.is_touched():
+                    letter.draw(YELLOW)
+                    print(letter.letter)
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 carryOn = False
@@ -86,28 +151,13 @@ while carryOn:
      # --- Game logic should go here
     mouse = pygame.mouse.get_pos()
      # --- Drawing code should go here
-     # First, clear the screen to white. 
-    screen.fill(BLACK)
-    #The you can draw different shapes and lines or add text to your background stage.
-    #pygame.draw.rect(screen, RED, [55, 200, 100, 70],0)
-    #pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    #pygame.draw.ellipse(screen, BLACK, [20,20,250,100], 2)
-    #if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-    #    pygame.draw.rect(screen,colour_light,[width/2,height/2,140,40])
-    #else:
-    #    pygame.draw.rect(screen,colour_dark,[width/2,height/2,140,40])
-    draw_word("WORDLE", 150, 2, 0, BR_GREEN)
-    draw_word("INPUT_", 150, 3, 0, BR_GREEN)
-    draw_word("PASS", 150, 4, 1, RED)
 
-    touch_spot(touch_spots)
-    # --- Go ahead and update the screen with what we've drawn.
-    #pygame.display.flip(
+    timer.draw()
+    if timer.timer_expired():
+        carryOn = False
+    
+    clock.tick(50)
     pygame.display.update()
-     
-     # --- Limit to 60 frames per second
-    clock.tick(60)
- 
-#Once we have exited the main program loop we can stop the game engine:
-# print(touch_spots)
+# -------- End of Main Program Loop
+
 pygame.quit()
